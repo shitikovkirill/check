dcb22f621b85514acbb4c5e1631eb0aac21947e5
Shitikov Kirill <shitikovkirillt@gmail.com>
2025-01-14 11:25:16 +0100

    Update exc

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,3 +1,4 @@
 class AppException(Exception):
     def __init__(self, msg=None, *args, **kwargs):
-        super().__init__(msg or self.__doc__, *args, **kwargs)
+        doc = self.__doc__ and self.__doc__.strip()
+        super().__init__(msg or doc, *args, **kwargs)
