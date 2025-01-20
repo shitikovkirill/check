b6883b422887c9ed0a2ea83c5893254ac79ffd25
Shitikov Kirill <shitikovkirillt@gmail.com>
2025-01-20 09:01:51 +0100

    Fix returned value

--- app/check/validators.py
+++ app/check/validators.py
@@ -1,15 +1,19 @@
 from typing import Annotated
 
-from pydantic import AfterValidator, BeforeValidator, condecimal
+from pydantic import AfterValidator, BeforeValidator, PlainSerializer, condecimal
 
 PriceField = condecimal(gt=0, decimal_places=2)
 PriceRestField = condecimal(ge=0, decimal_places=2)
 PriceDecimalToInt = Annotated[PriceField, AfterValidator(lambda v: v * 100)]
 PriceIntToDecimal = Annotated[
-    PriceField, BeforeValidator(lambda v: v / 100), AfterValidator(float)
+    PriceField,
+    BeforeValidator(lambda v: v / 100),
+    PlainSerializer(lambda x: float(x), return_type=float),
 ]
 PriceRestIntToDecimal = Annotated[
-    PriceRestField, BeforeValidator(lambda v: v / 100), AfterValidator(float)
+    PriceRestField,
+    BeforeValidator(lambda v: v / 100),
+    PlainSerializer(lambda x: float(x), return_type=float),
 ]
 
 Quantity = condecimal(gt=0, decimal_places=3)
